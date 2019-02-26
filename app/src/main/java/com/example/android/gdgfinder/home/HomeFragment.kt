a76dd23aad8b80b9d42726085b246a34259cbd24
Allison Hirata <41648848+allison-udacity@users.noreply.github.com>
2019-02-26 14:50:28 -0800

    starter code

---
+++ app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
@@ -0,0 +1,28 @@
+package com.example.android.gdgfinder.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import com.example.android.gdgfinder.R
+
+class HomeFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = HomeFragment()
+    }
+
+    private lateinit var viewModel: HomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.home_fragment, container, false)
+        viewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
+
+        return view
+    }
+}

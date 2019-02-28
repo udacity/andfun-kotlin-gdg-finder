2639a29db9884aae22bcdf685ad8b59153245637
Sean McQuillan <seanmcq@google.com>
2019-02-28 15:13:58 -0800

    Add basic nav.

--- app/src/main/java/com/example/android/gdgfinder/MainActivity.kt
+++ app/src/main/java/com/example/android/gdgfinder/MainActivity.kt
@@ -1,12 +1,45 @@
 package com.example.android.gdgfinder
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI.navigateUp
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.android.gdgfinder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        setupNavigation()
+    }
+
+    /**
+     * Called when the hamburger menu or back button are pressed on the Toolbar
+     *
+     * Delegate this to Navigation.
+     */
+    override fun onSupportNavigateUp() = navigateUp(findNavController(R.id.nav_host_fragment), binding.drawerLayout)
+
+    /**
+     * Setup Navigation for this Activity
+     */
+    private fun setupNavigation() {
+        // first find the nav controller
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        // then setup the action bar, tell it about the DrawerLayout
+        setupActionBarWithNavController(navController, binding.drawerLayout)
+
+
+        // finally setup the left drawer (called a NavigationView)
+        binding.navigationView.setupWithNavController(navController)
     }
 }

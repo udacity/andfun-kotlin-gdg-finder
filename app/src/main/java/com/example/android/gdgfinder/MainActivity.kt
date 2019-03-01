050ed5fc23e74d4882229723b77772804e3714ab
Sean McQuillan <seanmcq@google.com>
2019-02-28 16:48:05 -0800

    Adding some STYLE to the Toolbar

--- app/src/main/java/com/example/android/gdgfinder/MainActivity.kt
+++ app/src/main/java/com/example/android/gdgfinder/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.android.gdgfinder
 
 import android.os.Bundle
+import android.transition.TransitionManager
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI.navigateUp
 import androidx.navigation.ui.NavigationUI.setupWithNavController
@@ -35,11 +38,27 @@ class MainActivity : AppCompatActivity() {
         // first find the nav controller
         val navController = findNavController(R.id.nav_host_fragment)
 
+        setSupportActionBar(binding.toolbar)
+
         // then setup the action bar, tell it about the DrawerLayout
         setupActionBarWithNavController(navController, binding.drawerLayout)
 
 
         // finally setup the left drawer (called a NavigationView)
         binding.navigationView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination: NavDestination, _ ->
+            val toolBar = supportActionBar ?: return@addOnDestinationChangedListener
+            when(destination.id) {
+                R.id.home -> {
+                    toolBar.setDisplayShowTitleEnabled(false)
+                    binding.heroImage.visibility = View.VISIBLE
+                }
+                else -> {
+                    toolBar.setDisplayShowTitleEnabled(true)
+                    binding.heroImage.visibility = View.GONE
+                }
+            }
+        }
     }
 }

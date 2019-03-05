f7c86409e46a03ef47f6fba6321bcc81a0e54c4d
Sean McQuillan <seanmcq@google.com>
2019-03-04 16:16:43 -0800

    Start for home screen.

--- app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
+++ app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
@@ -1,12 +1,11 @@
 package com.example.android.gdgfinder.home
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.example.android.gdgfinder.R
 
 class HomeFragment : Fragment() {

3f3dd394c47623fd74aad343a3378859125547db
Sean McQuillan <seanmcq@google.com>
2019-03-07 16:36:11 -0800

    Add FAB + Click Handler

--- app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
+++ app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
@@ -5,8 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.NavDirections
+import androidx.navigation.findNavController
 import com.example.android.gdgfinder.R
+import com.example.android.gdgfinder.databinding.HomeFragmentBinding
 
 class HomeFragment : Fragment() {
 
@@ -20,13 +24,20 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.home_fragment, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+        val binding = HomeFragmentBinding.inflate(inflater)
         viewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+        binding.viewModel = viewModel
 
+        viewModel.navigateToSearch.observe(viewLifecycleOwner,
+            Observer<NavigateToSearch> { navigate ->
+                when (navigate) {
+                    NavigateToSearch -> {
+                        val navController = binding.root.findNavController()
+                        navController.navigate(R.id.action_homeFragment_to_gdgListFragment)
+                        viewModel.onNavigatedToSearch()
+                    }
+                }
+            })
+        return binding.root
+    }
 }
--- app/src/main/java/com/example/android/gdgfinder/home/HomeViewModel.kt
+++ app/src/main/java/com/example/android/gdgfinder/home/HomeViewModel.kt
@@ -1,7 +1,21 @@
 package com.example.android.gdgfinder.home
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel;
 
 class HomeViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val _navigateToSearch = MutableLiveData<NavigateToSearch>()
+    val navigateToSearch: LiveData<NavigateToSearch>
+        get() = _navigateToSearch
+
+    fun onFabClicked() {
+        _navigateToSearch.value = NavigateToSearch
+    }
+
+    fun onNavigatedToSearch() {
+        _navigateToSearch.value = null
+    }
 }
+
+object NavigateToSearch

47149cc8801d9084da6f70a3af888aa4b1c54cb7
Sean McQuillan <seanmcq@google.com>
2019-03-08 17:20:54 -0800

    Some minor refactors to follow thread confinement best practices.

--- app/src/main/java/com/example/android/gdgfinder/search/GdgChapterRepository.kt
+++ app/src/main/java/com/example/android/gdgfinder/search/GdgChapterRepository.kt
@@ -1,13 +1,19 @@
 package com.example.android.gdgfinder.search
 
 import android.location.Location
-import com.example.android.gdgfinder.network.*
-import kotlinx.coroutines.*
+import com.example.android.gdgfinder.network.GdgApiService
+import com.example.android.gdgfinder.network.GdgChapter
+import com.example.android.gdgfinder.network.GdgResponse
+import com.example.android.gdgfinder.network.LatLong
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.withContext
 
 class GdgChapterRepository(gdgApiService: GdgApiService) {
 
     private val request = gdgApiService.getChapters()
-    private var sortedData: SortedData? = null
     private var inProgressSort: Deferred<SortedData>? = null
 
     var isFullyInitialized = false
@@ -15,7 +21,9 @@ class GdgChapterRepository(gdgApiService: GdgApiService) {
 
 
     /**
-     * Get the chapters list for a specified filter
+     * Get the chapters list for a specified filter.
+     *
+     * This will be cancel if a new location is sent before the result is available.
      */
     suspend fun getChaptersForFilter(filter: String?): List<GdgChapter> {
         val data = completedData()
@@ -26,49 +34,65 @@ class GdgChapterRepository(gdgApiService: GdgApiService) {
     }
 
     /**
-     * Get the cached sorted data, the next computed sorted data, or finally start a new sort.
-     */
-    private suspend fun GdgChapterRepository.completedData() =
-        sortedData ?: inProgressSort?.await() ?: doSortData()
-
-    /**
-     * Get the filters sorted by distance from the last location
+     * Get the filters sorted by distance from the last location.
+     *
+     * This will cancel if a new location is sent before the result is available.
      */
     suspend fun getFilters(): List<String> = completedData().filters
+
+    /**
+     * Get the cached sorted data, the next computed sorted data, or finally start a new sort.
+     */
+    private suspend fun completedData(): SortedData = withContext(Dispatchers.Main) {
+        // We need to ensure we're on Dispatchers.Main so that this is not running on multiple Dispatchers and we
+        // modify the member inProgressSort.
+
+        // Since this was called from viewModelScope, that will always be a simple if check (not expensive), but
+        // by specifying the dispatcher we can protect against incorrect usage.
+
+        // if there's currently a sort running (or completed) wait for it to complete and return that value
+        // otherwise, start a new sort with no location (the user has likely not given us permission to use location
+        // yet)
+        inProgressSort?.await() ?: doSortData()
+    }
 
     /**
      * Call this to force a new sort to start.
      */
     private suspend fun doSortData(location: Location? = null): SortedData {
+        // since we'll need to launch a new coroutine for the sorting use coroutineScope.
+        // coroutineScope will automatically wait for anything started via async {} or await{} in it's block to
+        // complete.
         val result = coroutineScope {
-            // launch a new coroutine so other requests can wait for this sort to complete
-            // Also confine sorting to Dispatchres.Default because it's expensive
-            val deferred = async(Dispatchers.Default) {
-                SortedData(request.await(), location)
-            }
+            // launch a new coroutine to do the sort (so other requests can wait for this sort to complete)
+            val deferred = async { SortedData.from(request.await(), location) }
             // cache the Deferred so any future requests can wait for this sort
             inProgressSort = deferred
             // and return the result of this sort
             deferred.await()
         }
-        // cache the result so we don't have to call this all the time
-        sortedData = result
         return result
     }
 
     /**
-     * Call when location changes. This may cancel any previous queries, so it's important to re-request the data
-     * after sorting is complete.
+     * Call when location changes.
+     *
+     * This will cancel any previous queries, so it's important to re-request the data after calling this function.
      */
     suspend fun onLocationChanged(location: Location) {
-        isFullyInitialized = true
-        // previous sorts are now invalid, clear out the caches
-        sortedData = null
+        // We need to ensure we're on Dispatchers.Main so that this is not running on multiple Dispatchers and we
+        // modify the member inProgressSort.
 
-        // cancel any in progress sorts
-        inProgressSort?.cancel()
+        // Since this was called from viewModelScope, that will always be a simple if check (not expensive), but
+        // by specifying the dispatcher we can protect against incorrect usage.
+        withContext(Dispatchers.Main) {
+            isFullyInitialized = true
 
-        doSortData(location)
+            // cancel any in progress sorts, their result is not valid anymore.
+            inProgressSort?.cancel()
+
+            doSortData(location)
+        }
     }
 
     /**
@@ -77,30 +101,48 @@ class GdgChapterRepository(gdgApiService: GdgApiService) {
      * Note, by convention this class won't be instantiated on the Main thread. This is not a public API and should
      * only be called by [doSortData].
      */
-    private class SortedData(response: GdgResponse, location: Location?) {
-        val chapters: List<GdgChapter> = response.chapters.sortByDistanceFrom(location)
-        val filters: List<String> = chapters.map { it.region } .distinctBy { it }
+    private class SortedData private constructor(
+        val chapters: List<GdgChapter>,
+        val filters: List<String>,
+        val chaptersByRegion: Map<String, List<GdgChapter>>
+    ) {
 
-        var chaptersByRegion: Map<String, List<GdgChapter>> = chapters.groupBy { it.region }
+        companion object {
+            suspend fun from(response: GdgResponse, location: Location?): SortedData {
+                return withContext(Dispatchers.Default) {
+                    // this sorting is too expensive to do on the main thread, so do thread confinement here.
+                    val chapters: List<GdgChapter> = response.chapters.sortByDistanceFrom(location)
+                    // use distinctBy which will maintain the input order - this will have the effect of making
+                    // a filter list sorted by the distance from the current location
+                    val filters: List<String> = chapters.map { it.region } .distinctBy { it }
+                    // group the chapters by region so that filter queries don't require any work
+                    val chaptersByRegion: Map<String, List<GdgChapter>> = chapters.groupBy { it.region }
+                    // return the sorted result
+                    SortedData(chapters, filters, chaptersByRegion)
+                }
 
-        /**
-         * Sort a list of GdgChapter by their distance from the specified location.
-         *
-         * @param currentLocation returned list will be sorted by the distance, or unsorted if null
-         */
-        private fun List<GdgChapter>.sortByDistanceFrom(currentLocation: Location?): List<GdgChapter> {
-            currentLocation ?: return this
+            }
 
-            return sortedBy { distanceBetween(it.geo, currentLocation)}
-        }
 
-        /**
-         * Calculate the distance (in meters) between a LatLong and a Location.
-         */
-        private fun distanceBetween(start: LatLong, currentLocation: Location): Float {
-            val results = FloatArray(3)
-            Location.distanceBetween(start.lat, start.long, currentLocation.latitude, currentLocation.longitude, results)
-            return results[0]
+            /**
+             * Sort a list of GdgChapter by their distance from the specified location.
+             *
+             * @param currentLocation returned list will be sorted by the distance, or unsorted if null
+             */
+            private fun List<GdgChapter>.sortByDistanceFrom(currentLocation: Location?): List<GdgChapter> {
+                currentLocation ?: return this
+
+                return sortedBy { distanceBetween(it.geo, currentLocation)}
+            }
+
+            /**
+             * Calculate the distance (in meters) between a LatLong and a Location.
+             */
+            private fun distanceBetween(start: LatLong, currentLocation: Location): Float {
+                val results = FloatArray(3)
+                Location.distanceBetween(start.lat, start.long, currentLocation.latitude, currentLocation.longitude, results)
+                return results[0]
+            }
         }
     }
 }

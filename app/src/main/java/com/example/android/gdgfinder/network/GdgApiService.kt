def38f3afbfdf6ea3857aff56f2bfdf556af486c
SudKul <sudhanshu.kulshrestha@gmail.com>
2020-08-25 15:38:39 +0530

    Update backend server URL

--- app/src/main/java/com/example/android/gdgfinder/network/GdgApiService.kt
+++ app/src/main/java/com/example/android/gdgfinder/network/GdgApiService.kt
@@ -9,7 +9,7 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 
-private const val BASE_URL = "https://developers.google.com/programs/community/gdg/data/"
+private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com/"
 interface GdgApiService {
     @GET("gdg-directory.json")
     fun getChapters():

14de505c5f716f25b7f63cee8feb1f54cdd111b5
SudKul <sudhanshu.kulshrestha@gmail.com>
2020-08-25 15:23:44 +0530

    Update app/build.gradle for backend server URL

--- app/src/main/java/com/example/android/gdgfinder/network/GdgApiService.kt
+++ app/src/main/java/com/example/android/gdgfinder/network/GdgApiService.kt
@@ -9,9 +9,9 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 
-private const val BASE_URL = "https://developers.google.com/community/gdg/directory/"
+private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com/"
 interface GdgApiService {
-    @GET("directory.json")
+    @GET("gdg-directory.json")
     fun getChapters():
     // The Coroutine Call Adapter allows us to return a Deferred, a Job with a result
             Deferred<GdgResponse>
